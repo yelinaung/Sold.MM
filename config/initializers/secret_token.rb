0519d0ebca785a4509f72fb3f58f13aa33fa2135
mgpyone <mgpyone@mysteryzillion.com>
2013-02-10 16:53:55 +0630

    first commit after cleaning the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoldMm::Application.config.secret_token = 'cd550143334c7d270975fb46b2d23621230071f2b8e84a75320479c7ba1074a9f6499a3b8c7c0f233e6b5e52773d23749db48fa5d8c20815a031af181fd3304a'
